1258b9bae15b4c0af66616efc86df27a3845448b
superJCZ <jczhuscu@163.com>
2022-10-24 23:44:46 +0800

    10.24

--- 数组/35搜索插入位置.py
+++ 数组/35搜索插入位置.py
@@ -4,32 +4,7 @@ from typing import List
 
 
 class Solution:
-
-    # 应该左开右闭的方法：左闭右闭时最后一个指到最后一个元素时无法确定插入位置在元素的左边还是右边
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        l,r,mid = 0,len(nums)-1,0
-
-        # len(nums) = 1
-        if len(nums) == 1:
-            return 0 if target < nums[0] or target == nums[0] else 1
-
-        # len(nums) != 1
-        else:
-            ## target < min(nums) or target > max(nums)
-            if target < min(nums) or target > max(nums):
-                return 0 if target < nums[0] else len(nums)
-
-            ## target between min(nums) and max(nums)
-            else:
-                while l < r:
-                    mid = (l+r)//2
-                    if target == nums[mid]:
-                        return mid
-                    elif target < nums[mid]:
-                        l = mid
-                    else:
-                        r = mid
-                return r
+    pass
 
 
 if __name__ == "__main__":
